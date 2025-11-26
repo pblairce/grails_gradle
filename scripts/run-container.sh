6a0ef61a141679e4ef039842f19a56d201141dfe
Blair Patrick pjblair <pjblair@uwe.nsa.gov>
2025-11-26 15:20:59 +0000

    got the container working

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f build/compose.yaml run --rm --name grails_gradle grails_gradle ash
+docker compose -f build/compose.yaml run --rm --name gradle_grails gradle_grails bash
