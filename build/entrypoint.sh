445ed3dced320c569728cb2c4bf34fba93a13763
Blair Patrick pjblair <pjblair@uwe.nsa.gov>
2025-11-26 15:39:44 +0000

    fixing entry point and removing .gradle

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 
 if [[ -f /etc/pki/tls/certs/ca.crt ]]; then
     keytool -import -alias neoforge -file /etc/pki/tls/certs/ca.crt -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt
